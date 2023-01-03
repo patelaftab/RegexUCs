8db137a6c560f0e166538e73092a9254eac1af13
patelaftab <10patelaftab@gmail.com>
2023-01-03 19:46:10 +0530

    [Aftab] Validating a User Name

---
+++ RegexUCs/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RegexUCs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registration Program\n");
+            Console.WriteLine("Choose a VAlid Option From Below\n" +
+                "1]: For User Name validation");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option) 
+            {
+                case 1:
+                    UserRegistration.FirstName();
+                    break;
+                default:
+                    Console.WriteLine("Choose a Valid Option");
+                    break;
+            }
+          
+        }
+    }
+}
